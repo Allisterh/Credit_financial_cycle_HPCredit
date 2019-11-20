0951f17a7fa272e43ade0944ca21b058c19870c5
nam400 <nam400@gmail.com>
2019-11-19 23:17:55 -0600

    Changes on comments

--- Data Collection.R
+++ Data Collection.R
@@ -1,7 +1,12 @@
 #Data Import Here
+  #Import Credit Data
+  #Import HP data
 
 #Cleanup and organize data for regression
+  #HP filter to get the trend data
+    #read up on how to create this gap variable
 
 #Regression
+  #Logistic regression
 
-#Summary with charts
+#Summary with chart
